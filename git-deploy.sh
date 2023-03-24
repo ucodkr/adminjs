04cc41da133c870633e503ac6a201e4a9da77712
ds <a@xx.com>
2023-03-24 18:31:33 +0900

    aaa

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+TAG=v6.8.7-ucod-1
 git checkout npm-deploy
+git merge ucodkr
+yarn npm-release
+git add lib types -f
+git commit -m "${TAG}"
+git push origin npm-deploy
+
+git tag -d ${TAG}
+git push --delete origin ${TAG}
+git tag ${TAG}
+git push origin ${TAG}
